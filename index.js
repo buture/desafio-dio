d727d816882ae2aeb67e71976947b4681c1ed73d
buture <buture@gmail.com>
2024-01-31 16:11:50 -0300

    setando as variaveis e criando os blocos de repetição

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-Desafio
+let nomeDoHeroi = "Superman";
+let experienciaDoHeroi = 5000;
+let nivelDoHeroi;
+if (experienciaDoHeroi < 1000) {
+  nivelDoHeroi = "Ferro";
+} else if (experienciaDoHeroi >= 1001 && experienciaDoHeroi <= 2000) {
+  nivelDoHeroi = "Bronze";
+} else if (experienciaDoHeroi >= 2001 && experienciaDoHeroi <= 5000) {
+  nivelDoHeroi = "Prata";
+} else if (experienciaDoHeroi >= 5001 && experienciaDoHeroi <= 7000) {
+  nivelDoHeroi = "Ouro";
+} else if (experienciaDoHeroi >= 7001 && experienciaDoHeroi <= 8000) {
+  nivelDoHeroi = "Platina";
+} else if (experienciaDoHeroi >= 8001 && experienciaDoHeroi <= 9000) {
+  nivelDoHeroi = "Ascendente";
+} else if (experienciaDoHeroi >= 9001 && experienciaDoHeroi <= 10000) {
+  nivelDoHeroi = "Imortal";
+} else {
+  nivelDoHeroi = "Radiante";
+}
